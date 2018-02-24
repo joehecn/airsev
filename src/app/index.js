13ed16c90bf86b47785726ce266f890ffefdad87
joehe <leanbrown@live.cn>
2018-02-24 16:40:54 +0800

    add kcors

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,6 @@
 
 const Koa = require('koa')
+const cors = require('kcors')
 // const xmlParser = require('koa-xml-body')
 const bodyParser = require('koa-bodyparser')
 const router = require('./router')
@@ -7,6 +8,7 @@ const router = require('./router')
 const koa = module.exports = new Koa()
 
 koa
+  .use(cors())
   // .use(xmlParser({
   //   xmlOptions: {
   //     explicitArray: false
